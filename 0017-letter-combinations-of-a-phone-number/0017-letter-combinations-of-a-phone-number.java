b5a54f42930fb05c5e51e666665a5b5902b2c89c
Rohan <rohansinghbsr001@gmail.com>
2026-08-08 13:05:27 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.62%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,38 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
 
-        if(digits.length()==0){
-            return Collections.emptyList();
-        }
         List<String> list = new ArrayList<>();
 
-        String[] stringArr={"", "", "abc","def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        int index=0;
+        if(digits.length()==0){
+            return list;
+        }
 
-        solve("", digits, index, list, stringArr);
+        String[] stringArr={"", "", "abc","def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        StringBuilder currentStr=new StringBuilder(); 
+
+        solve(currentStr, digits, 0, list, stringArr);
         return list;
     }
 
-    private void solve(String combination, String digits, int index, List<String> list, String[] stringArr){
+    private void solve(StringBuilder currentStr, String digits, int index, List<String> list, String[] stringArr){
 
         if(index>=digits.length()){
-            list.add(combination);
+            list.add(currentStr.toString());
         }
 
         else{
             int number=digits.charAt(index)-'0';
             String value=stringArr[number];
 
-            for(char letter : value.toCharArray()){
-                solve(combination+letter,digits,index+1,list,stringArr);
+            for(int i=0;i<value.length();i++){
+
+                char selectedChar = value.charAt(i);
+
+                currentStr.append(selectedChar);
+
+                solve(currentStr,digits,index+1,list,stringArr);
+                currentStr.deleteCharAt(currentStr.length() - 1);
             }
         }
     }
