5b98e8e3db8b8a8089ce8622c4c0b43a70002008
Rohan <rohansinghbsr001@gmail.com>
2026-08-09 17:15:22 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,31 +1,50 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-    int i=0;
-    int j=0;
-    int count=0;
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
 
-    while(i<nums1.length &&j<n){
-
-        if(i>=m+count && nums1[i]==0){
-            nums1[i]=nums2[j];
-            i++;
-            j++;
-        }
-        else if(nums1[i]<nums2[j]){
-            i++;
-        }
-
-        else{
-            for(int k=m+j-1;k>=i;k--){
-                nums1[k+1]=nums1[k];
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
-            nums1[i]=nums2[j];
-            i++;
-            j++;
-            count++;
+            else{
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
         }
-    }
+
+      //this is my first approch and after submission i seen best approch then i aply 
+
+      // int i=0;
+      // int j=0;
+      // int count=0;
+
+      // while(i<nums1.length &&j<n){
+
+      //     if(i>=m+count && nums1[i]==0){
+      //         nums1[i]=nums2[j];
+      //         i++;
+      //         j++;
+      //     }
+      //     else if(nums1[i]<nums2[j]){
+      //         i++;
+      //     }
+
+      //     else{
+      //         for(int k=m+j-1;k>=i;k--){
+      //             nums1[k+1]=nums1[k];
+      //         }
+      //         nums1[i]=nums2[j];
+      //         i++;
+      //         j++;
+      //         count++;
+      //     }
+      // }
         
     }
 }
