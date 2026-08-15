fd0ff4aa23aa44e71e5d146a04870ec60ce6f144
Rohan <rohansinghbsr001@gmail.com>
2026-08-15 15:56:38 +0530

    Time: 0 ms (100%), Space: 47.2 MB (66.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans=nums.length;
+        //this is my first approch becasue i now how XOR operator works
+
+        // int ans=nums.length;
+        // for(int i=0;i<nums.length;i++){
+        //     ans=i^ans^nums[i];
+        // }
+        // return ans;
+
+        //this is my second thought(approch) i now this concept also 
+        int totalSum=nums.length*(nums.length+1)/2;
+        int sum=0;
         for(int i=0;i<nums.length;i++){
-            ans=i^ans^nums[i];
+            sum+=nums[i];
         }
-        return ans;
+
+        return totalSum-sum;
     }
 }
