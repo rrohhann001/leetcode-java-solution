cef8a56a38ee88331364bafc0bea892700891ed0
Rohan <rohansinghbsr001@gmail.com>
2026-08-15 18:35:02 +0530

    Time: 0 ms (100%), Space: 45.1 MB (21.6%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,36 +1,55 @@
 class Solution {
     public int findMin(int[] nums) {
 
-        if(nums[0]<nums[nums.length-1]){
-            return nums[0];
-        }
+        int lb=0;
+        int ub=nums.length-1;
 
-        int ans=binarySearch(nums[0], 0,nums.length-1,nums);
-        return ans;
-    }    
-
-    private int binarySearch(int min, int lb, int ub, int[] nums){
-        
-        while(lb<=ub){
+        while(lb<ub){
             int mid=lb+(ub-lb)/2;
-            if(nums[mid]<min || mid>1 && nums[mid-1]<min){
-                min=nums[mid];
-                ub=mid-1;
-            }
 
-            else {
+            if(nums[mid]>nums[ub]){
                 lb=mid+1;
             }
-        }
-        if(min<nums[0] || ub==0){
-            return min;
-        }
-        else if(ub>1){
-            min= binarySearch(min,0,ub/2-1,nums);
-        }
-        
 
-        return min;
-        
+            else if(nums[mid]<nums[ub]){
+                ub=mid;
+            }
+
+            else{
+                ub--;
+            }
+        }
+
+        return nums[lb];
     }
+
+        //my first approch and logic beats 100% but according  to me it no satisfy all conditions
+
+    //     if(nums[0]<nums[nums.length-1]){
+    //         return nums[0];
+    //     }
+
+    //     int ans=binarySearch(0,nums.length-1,nums);
+    //     if(ans<nums[0]){
+    //         return ans;
+    //     }
+
+    //     return binarySearch(0,((nums.length-1)/2)-1,nums);
+    // }    
+
+    // private int binarySearch(int lb, int ub, int[] nums){
+    //     int min=nums[0];
+    //     while(lb<=ub){
+    //         int mid=lb+(ub-lb)/2;
+    //         if(nums[mid]<min ||mid>0 &&nums[mid-1]<min){
+    //             min=nums[mid];
+    //             ub=mid-1;
+    //         }
+
+    //         else {
+    //             lb=mid+1;
+    //         }
+    //     }
+    //     return min;
+    // }
 }
