57c1d9c2d71748a415ff58407f162ca25e55f624
Rohan <rohansinghbsr001@gmail.com>
2026-08-19 22:07:09 +0530

    Time: 3 ms (99.82%), Space: 94.6 MB (36.66%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -13,38 +13,85 @@ class Solution {
         if(head==null || head.next==null){
             return true;
         }
-        ListNode pre=null;
-        ListNode current=head;
-        ListNode next=current.next;
-        ListNode temp=head;
-        StringBuilder str1=new StringBuilder();
-        StringBuilder str2=new StringBuilder();
 
-        while(temp!=null){
-            str1.append(temp.val);
-            temp=temp.next;
+        ListNode pre=null;
+        ListNode slow=head;
+        ListNode fast=head;
+    
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+
+            //rev
+            ListNode nextNode=slow.next;
+            slow.next=pre;
+            pre=slow;
+            slow=nextNode;    
         }
-        
-        while(current!=null){
-            current.next=pre;
-            pre=current;
-            current=next;
-            if(next!=null){
-                next=next.next;
+
+        //In case of even linked list 
+        if(fast==null){
+            while(pre!=null){
+                if(pre.val!=slow.val){
+                    return false;
+                }
+                pre=pre.next;
+                slow=slow.next;
             }
         }
 
-        while(pre!=null){
-            str2.append(pre.val);
-            pre=pre.next;
+        //In case of odd linked list, move the slow pointer by 1 step
+        else{
+            slow=slow.next;
+            while(pre!=null){
+                if(pre.val!=slow.val){
+                    return false;
+                }
+                pre=pre.next;
+                slow=slow.next;
+            }
         }
 
-        if(str1.toString().equals(str2.toString())){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
+
+
+
+
+
+
+
+        //this is my first approch, that takes more time and space complexity 
+        // ListNode pre=null;
+        // ListNode current=head;
+        // ListNode next=current.next;
+        // ListNode temp=head;
+        // StringBuilder str1=new StringBuilder();
+        // StringBuilder str2=new StringBuilder();
+
+        // while(temp!=null){
+        //     str1.append(temp.val);
+        //     temp=temp.next;
+        // }
+        
+        // while(current!=null){
+        //     current.next=pre;
+        //     pre=current;
+        //     current=next;
+        //     if(next!=null){
+        //         next=next.next;
+        //     }
+        // }
+
+        // while(pre!=null){
+        //     str2.append(pre.val);
+        //     pre=pre.next;
+        // }
+
+        // if(str1.toString().equals(str2.toString())){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
 
     }
 }
