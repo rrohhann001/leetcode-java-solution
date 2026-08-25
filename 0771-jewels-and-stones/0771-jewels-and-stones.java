76d9f37d65873955ce7ad5081222651bf33dd4ba
Rohan <rohansinghbsr001@gmail.com>
2026-08-25 15:05:33 +0530

    Time: 0 ms (100%), Space: 43.2 MB (54.48%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,17 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        //Kyuki standard ASCII characters ke codes 0 se 127 tak hote hain.
-        boolean[] isJewels=new boolean[128];
-        for(int i=0;i<jewels.length();i++){
-            isJewels[jewels.charAt(i)]=true; //kyoki initaily all are false
-        }
-        int count=0;
-        for(int i=0;i<stones.length();i++){
-            if(isJewels[stones.charAt(i)]){
-                count++;
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            for (int j = 0; j < jewels.length(); j++) {
+                if (stones.charAt(i) == jewels.charAt(j)) {
+                    count++;
+                    break;
+                }
             }
         }
         return count;
+    
+
 
 
 
