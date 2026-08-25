d6d2e6ae4f7e73fe529d74dab0aafc0cd3be8d97
Rohan <rohansinghbsr001@gmail.com>
2026-08-25 15:09:58 +0530

    Time: 0 ms (100%), Space: 43.3 MB (20.31%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,28 +1,27 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-        for (int i = 0; i < stones.length(); i++) {
-            for (int j = 0; j < jewels.length(); j++) {
-                if (stones.charAt(i) == jewels.charAt(j)) {
-                    count++;
-                    break;
-                }
+        //Kyuki standard ASCII characters ke codes 0 se 127 tak hote hain.
+        boolean[] isJewels=new boolean[128];
+        for(int i=0;i<jewels.length();i++){
+            isJewels[jewels.charAt(i)]=true; //kyoki initaily all are false
+        }
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(isJewels[stones.charAt(i)]){
+                count++;
             }
         }
         return count;
-    
-
 
 
 
 
         //my first approch beats 100% and runtime 0 but above is more efficient code
-        // int count=0;
-        // for(int i=0;i<jewels.length();i++){
-        //     char ch=jewels.charAt(i);
-        //     for(int j=0;j<stones.length();j++){
-        //         if(ch==stones.charAt(j)){
+        //for (int i = 0; i < stones.length(); i++) {
+        //     for (int j = 0; j < jewels.length(); j++) {
+        //         if (stones.charAt(i) == jewels.charAt(j)) {
         //             count++;
+        //             break;
         //         }
         //     }
         // }
