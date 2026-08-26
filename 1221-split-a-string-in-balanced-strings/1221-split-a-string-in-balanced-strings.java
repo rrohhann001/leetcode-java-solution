fe8cf8e6aaba9068d3a5128603c92172acbde32b
Rohan <rohansinghbsr001@gmail.com>
2026-08-26 16:40:36 +0530

    Time: 0 ms (100%), Space: 43 MB (9.2%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,20 +1,44 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        Stack<Character> st=new Stack<>();
         int count=0;
-        for(int i=0;i<s.length();i++){
-            if(st.isEmpty()){
-                st.push(s.charAt(i));
+        int balance=0;
+        char ch=s.charAt(0);
+        for(int i=0;i<s.length(); i++){
+            
+            if(balance==0){
+                ch = s.charAt(i);
+                balance++;
                 count++;
             }
-            else if(st.peek()==s.charAt(i)){
-                st.push(s.charAt(i));
+            else if(ch==s.charAt(i)){
+                balance++;
             }
             else{
-                st.pop();
+                balance--;
             }
         }
+
         return count;
+
+
+
+
+        //my first approch take 2ms time and above is best approch  
+        // Stack<Character> st=new Stack<>();
+        // int count=0;
+        // for(int i=0;i<s.length();i++){
+        //     if(st.isEmpty()){
+        //         st.push(s.charAt(i));
+        //         count++;
+        //     }
+        //     else if(st.peek()==s.charAt(i)){
+        //         st.push(s.charAt(i));
+        //     }
+        //     else{
+        //         st.pop();
+        //     }
+        // }
+        // return count;
         
     }
 }
