f57b92cfc509803a01ae08c6e91c8d44d45e55d2
Rohan <rohansinghbsr001@gmail.com>
2026-08-27 21:25:40 +0530

    Time: 2 ms (99.3%), Space: 44.8 MB (42.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,10 +4,10 @@ class Solution {
         int i=0;
         int j=arr.length-1;
         while(i<j){
-            if(arr[i]<48 || arr[i]>57 && arr[i]<97 || arr[i]>122){
+            if(arr[i]<'0' || arr[i]>'9' && arr[i]<'a' || arr[i]>'z'){
                 i++;
             }
-            else if(arr[j]<48 || arr[j]>57 &&arr[j]<97 || arr[j]>122){
+            else if(arr[j]<'0' || arr[j]>'9' &&arr[j]<'a' || arr[j]>'z'){
                 j--;
             }
             else if(arr[i]!=arr[j]){
