566e4102a4af50433f19d0e0db5a233f25614373
Rohan <rohansinghbsr001@gmail.com>
2026-08-29 11:44:51 +0530

    Time: 5 ms (62.41%), Space: 44.2 MB (82.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,14 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] arrS=new int[26];
-        int[] arrT=new int[26];
+        int[] arr=new int[26];
         for(int i=0; i<s.length();i++){
-            char chS=s.charAt(i);
-            char chT=t.charAt(i);
-            arrS[chS-'a']++;
-            arrT[chT-'a']++;           
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;           
         }
         
         for(int i=0;i<26;i++){
-            if(arrS[i]!=arrT[i]){
+            if(arr[i]!=0){
                 return false;
             }
         }
