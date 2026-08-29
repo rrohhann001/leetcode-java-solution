e73a3d9b437973363f6448f630c43c6cf8fab1c1
Rohan <rohansinghbsr001@gmail.com>
2026-08-29 12:04:04 +0530

    Time: 5 ms (62.41%), Space: 44.4 MB (74.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
+    public static boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()){
             return false;
         }
