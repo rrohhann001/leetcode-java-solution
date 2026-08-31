487078c721094f89098301354c9bfce2a9603e44
Rohan <rohansinghbsr001@gmail.com>
2026-08-31 17:12:54 +0530

    Time: 3 ms (28.8%), Space: 43.9 MB (11.26%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,12 +1,23 @@
 class Solution {
     public int secondHighest(String s) {
         ArrayList<Integer> arr=new ArrayList<>();
-
+        int firstLargest=-1;
+        int secondLargest=-1;
+        int j=-1;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
 
             if(ch<'a'){
                 arr.add(ch-'0');
+                j++;
+
+                if(arr.get(j)>firstLargest){
+                    secondLargest=firstLargest;
+                    firstLargest=arr.get(j);
+                }
+                else if(arr.get(j)>secondLargest && arr.get(j)<firstLargest){
+                    secondLargest=arr.get(j);
+                }
             }
             
             // switch(s.charAt(i)){
@@ -54,19 +65,7 @@ class Solution {
         if(arr.size()==0){
             return -1;
         }
-        int firstLargest=arr.get(0);
-        int secondLargest=-1;
-        for(int i=0;i<arr.size();i++){
-
-            if(arr.get(i)>firstLargest){
-                secondLargest=firstLargest;
-                firstLargest=arr.get(i);
-            }
-            else if(arr.get(i)>secondLargest && arr.get(i)<firstLargest){
-                secondLargest=arr.get(i);
-            }
-        }
-
+        
         return secondLargest;
     }
 }
