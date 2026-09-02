748769ae0cfdf81d5f2af6bcffce269401b12777
Rohan <rohansinghbsr001@gmail.com>
2026-09-02 17:46:21 +0530

    Time: 4 ms (93.08%), Space: 44.4 MB (57.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,21 +1,53 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] arr=s.split(" ");
         StringBuilder rev=new StringBuilder();
-
-        for(int i=arr.length-1;i>=0;i--){
-            if(arr[i].length()==0){
-                continue;
+        int startIndex=s.length()-1;
+        while(startIndex>=0){
+            while(startIndex>=0 && s.charAt(startIndex)==' '){
+                startIndex--;
             }
-            else if(rev.length()==0){
-                rev.append(arr[i]);
+            if(startIndex<0){
+                break;
+            }
+
+            int endIndex=startIndex;
+            while(startIndex>=0 && s.charAt(startIndex)!=' '){
+                startIndex--;
+            }
+
+            if(rev.length()==0){
+                rev.append(s.substring(startIndex+1,endIndex+1));
             }
             else{
                 rev.append(" ");
-                rev.append(arr[i]);
+                rev.append(s.substring(startIndex+1,endIndex+1));
             }
+            
         }
 
         return rev.toString();
+
+
+
+
+        // this is my first approch that beats 84.87% and runtime 5ms. 
+        // and in thius approch I use in build method split() of String. 
+        // String[] arr=s.split(" ");
+        // StringBuilder rev=new StringBuilder();
+
+        // for(int i=arr.length-1;i>=0;i--){
+        //     if(arr[i].length()==0){
+        //         continue;
+        //     }
+        //     else if(rev.length()==0){
+        //         rev.append(arr[i]);
+        //     }
+        //     else{
+        //         rev.append(" ");
+        //         rev.append(arr[i]);
+        //     }
+        // }
+
+        // return rev.toString();
     }
 }
