0717f07317eb7f9cfc1cd074ada8da583b26ba92
Rohan <rohansinghbsr001@gmail.com>
2026-09-03 18:44:45 +0530

    Time: 2 ms (91.84%), Space: 47.8 MB (38.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,11 +3,18 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
+                // int temp=nums[i];
+                // nums[i]=nums[j];
+                // nums[j]=temp;
+
+                //both are good but this(uncommented) is faster then commented logic
+                nums[j]=nums[i];
                 j++;
             } 
+        }
+        while(j<nums.length){
+            nums[j]=0;
+            j++;
         }  
     }
 }
