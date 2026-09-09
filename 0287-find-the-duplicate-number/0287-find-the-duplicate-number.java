1f6643b360c4a4beb38762975f2fd18614ef289c
Rohan <rohansinghbsr001@gmail.com>
2026-09-09 18:20:46 +0530

    Time: 5 ms (62.22%), Space: 82.9 MB (47.76%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        //this is 28% and runtime 22ms. but this is not valid answer because this takes O(n) apace that does not follows the constraints(rules)
         // HashMap<Integer,Integer> map=new HashMap<>();
         // for(int i=0;i<nums.length;i++){
         //     if(map.containsKey(nums[i])){
@@ -10,13 +11,34 @@ class Solution {
         // }
         // return 0;
 
-        int i=nums[0];
-        while(nums[i]>0){
-            int temp=nums[i];
-            nums[i]=-nums[i];
-            i=temp;
+
+        //this is 94% and runtime 3ms. but this is not valid answer because this modify the array that does not follows the constraints(rules)
+        // int i=nums[0];
+        // while(nums[i]>0){
+        //     int temp=nums[i];
+        //     nums[i]=-nums[i];
+        //     i=temp;
+        // }
+        // return i;
+
+
+        // this is the best approch that follows constraints
+        int slow=0;
+        int fast=0;
+        int n=nums.length;
+        while(fast!=n+1 && nums[fast]!=n+1){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast){
+                break;
+            }
         }
-        return i;
+        slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
         
     }
 }
