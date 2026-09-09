208732fbf57e7eebf3ed083bc2faea2fe57132b6
Rohan <rohansinghbsr001@gmail.com>
2026-09-09 18:27:58 +0530

    Time: 5 ms (62.22%), Space: 82.9 MB (47.76%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -23,16 +23,29 @@ class Solution {
 
 
         // this is the best approch that follows constraints
+        //floyed cycle detection(FCD) approch , means same as find starting cycle point in linked list 
+
+
+        //detect the common node
         int slow=0;
         int fast=0;
-        int n=nums.length;
-        while(fast!=n+1 && nums[fast]!=n+1){
+        // int n=nums.length;
+        // while(fast!=n+1 && nums[fast]!=n+1){  ye bhi theek hai lekin
+        //     slow=nums[slow];
+        //     fast=nums[nums[fast]];
+        //     if(slow==fast){
+        //         break;
+        //     }
+        // }
+
+        // ye jyada best hai 
+        do{
             slow=nums[slow];
             fast=nums[nums[fast]];
-            if(slow==fast){
-                break;
-            }
-        }
+        }while(slow!=fast);
+        
+
+        // lets find the start of the linked list
         slow=0;
         while(slow!=fast){
             slow=nums[slow];
