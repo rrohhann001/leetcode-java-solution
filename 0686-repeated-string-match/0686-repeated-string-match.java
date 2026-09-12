9fe6aa327cb34dbce83ac977f61132bc4a636305
Rohan <rohansinghbsr001@gmail.com>
2026-09-12 19:39:11 +0530

    Time: 262 ms (55.61%), Space: 43.7 MB (60.76%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb=new StringBuilder(a);
+        int count=1;
+        while(sb.length()<b.length()){
+            count++;
+            sb.append(a);     
+        }
+
+        if(sb.toString().contains(b)){
+            return count;
+        }
+        sb.append(a);
+        count++;
+        if(sb.toString().contains(b)){
+            return count;
+        }
+        return -1;
+
+    }
+}
