d714ca6a69392213442d1fe0ab8bfed153df7e50
Rohan <rohansinghbsr001@gmail.com>
2026-09-12 18:48:28 +0530

    Time: 17 ms (42.68%), Space: 56.2 MB (93.49%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,23 +1,48 @@
 class Solution {
     public int minSwaps(String s) {
-        Stack<Character> st=new Stack<>();
+
+        //this is my first approch with stack that beats 8.77% and runtime 124ms 
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(st.isEmpty()){
+        //         st.push(s.charAt(i));
+        //     }
+        //     else if(s.charAt(i)=='['){
+        //         st.push(s.charAt(i));
+        //     }
+        //     else{
+        //         st.pop();
+        //     }
+        // }
+        // int count=0;
+        // while(!st.isEmpty()){
+        //     st.pop();
+        //     count++;
+        // }
+
+        // return count/2;
+
+
+        int balance=0;
+        int count=0;
         for(int i=0;i<s.length();i++){
-            if(st.isEmpty()){
-                st.push(s.charAt(i));
+            char ch=s.charAt(i);
+            if(ch=='['){
+                balance++;
             }
-            else if(s.charAt(i)=='['){
-                st.push(s.charAt(i));
+            else if(balance>0){
+                balance--;
             }
             else{
-                st.pop();
+                count++;
             }
         }
-        int count=0;
-        while(!st.isEmpty()){
-            st.pop();
-            count++;
-        }
 
-        return count/2;
+        int ans=(balance+count)/2;
+        int countOfTwoInAns=ans/2;
+        if(ans%2==0){
+            return countOfTwoInAns;
+        }
+        return countOfTwoInAns+1;
     }
 }
