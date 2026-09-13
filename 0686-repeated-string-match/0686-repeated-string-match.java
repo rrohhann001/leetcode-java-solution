604fc8581bc1bc2ea29ac4ad6879b4865dd0aec5
Rohan <rohansinghbsr001@gmail.com>
2026-09-13 14:51:44 +0530

    Time: 4 ms (98.71%), Space: 43.5 MB (85.51%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,22 +1,7 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         //this is my first approch that beats 55% and runtime 262ms
-        // StringBuilder sb=new StringBuilder(a);
-        // int count=1;
-        // while(sb.length()<b.length()){
-        //     count++;
-        //     sb.append(a);     
-        // }
-
-        // if(sb.toString().contains(b)){
-        //     return count;
-        // }
-        // sb.append(a);
-        // count++;
-        // if(sb.toString().contains(b)){
-        //     return count;
-        // }
-        // return -1;
+        // 
 
 
         //
@@ -33,19 +18,20 @@ class Solution {
             }
         }
 
-        int repeatedCount=lengthB/lengthA;
-        int count=repeatedCount;
-        StringBuilder repeatedString=new StringBuilder();
-        while(count>0){
-            repeatedString.append(a);
-            count--;
+        StringBuilder sb=new StringBuilder(a);
+        int count=1;
+        while(sb.length()<b.length()){
+            count++;
+            sb.append(a);     
         }
-        for(int i=1;i<=3;i++){
-            if(repeatedString.toString().contains(b)){
-                return repeatedCount;
-            }
-            repeatedString.append(a);
-            repeatedCount++;
+
+        if(sb.toString().contains(b)){
+            return count;
+        }
+        sb.append(a);
+        count++;
+        if(sb.toString().contains(b)){
+            return count;
         }
         return -1;
 
