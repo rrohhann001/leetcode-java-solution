4cb75c5d1d6fb9be8ec86af501d71011c30f5ddb
Rohan <rohansinghbsr001@gmail.com>
2026-09-13 14:50:11 +0530

    Time: 4 ms (98.71%), Space: 43.3 MB (96.47%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,21 +1,56 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int count = (int) Math.ceil((double) b.length() / a.length());
+        //this is my first approch that beats 55% and runtime 262ms
+        // StringBuilder sb=new StringBuilder(a);
+        // int count=1;
+        // while(sb.length()<b.length()){
+        //     count++;
+        //     sb.append(a);     
+        // }
 
-        StringBuilder repeated = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            repeated.append(a);
+        // if(sb.toString().contains(b)){
+        //     return count;
+        // }
+        // sb.append(a);
+        // count++;
+        // if(sb.toString().contains(b)){
+        //     return count;
+        // }
+        // return -1;
+
+
+        //
+        int lengthA=a.length();
+        int lengthB=b.length();
+        boolean[] temp=new boolean[26];
+        for(int i=0;i<a.length();i++){
+            temp[a.charAt(i)-'a']=true;
         }
 
-        if (repeated.toString().contains(b)) {
-            return count;
+        for(int i=0;i<b.length();i++){
+            if(!temp[b.charAt(i)-'a']){
+                return -1;
+            }
         }
 
-        repeated.append(a);
-        if (repeated.toString().contains(b)) {
-            return count + 1;
+        int repeatedCount=lengthB/lengthA;
+        int count=repeatedCount;
+        StringBuilder repeatedString=new StringBuilder();
+        while(count>0){
+            repeatedString.append(a);
+            count--;
         }
-
+        for(int i=1;i<=3;i++){
+            if(repeatedString.toString().contains(b)){
+                return repeatedCount;
+            }
+            repeatedString.append(a);
+            repeatedCount++;
+        }
         return -1;
+
+
+
+
     }
 }
