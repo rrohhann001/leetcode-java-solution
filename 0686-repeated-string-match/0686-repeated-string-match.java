41b1cc36cf1314df0cf4f3523eba48b0a94eb26e
Rohan <rohansinghbsr001@gmail.com>
2026-09-13 14:54:40 +0530

    Time: 4 ms (98.71%), Space: 43.5 MB (85.51%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,12 +1,24 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         //this is my first approch that beats 55% and runtime 262ms
-        // 
+        // StringBuilder sb=new StringBuilder(a);
+        // int count=1;
+        // while(sb.length()<b.length()){
+        //     count++;
+        //     sb.append(a);     
+        // }
 
+        // if(sb.toString().contains(b)){
+        //     return count;
+        // }
+        // sb.append(a);
+        // count++;
+        // if(sb.toString().contains(b)){
+        //     return count;
+        // }
+        // return -1;
 
-        //
-        int lengthA=a.length();
-        int lengthB=b.length();
+        //this is my second approch that beats 98.71% and runtime is 4ms
         boolean[] temp=new boolean[26];
         for(int i=0;i<a.length();i++){
             temp[a.charAt(i)-'a']=true;
@@ -29,9 +41,8 @@ class Solution {
             return count;
         }
         sb.append(a);
-        count++;
         if(sb.toString().contains(b)){
-            return count;
+            return count+1;
         }
         return -1;
 
