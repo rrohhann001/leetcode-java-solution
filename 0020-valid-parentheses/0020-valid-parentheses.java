ef8f0af2faea2e2d5068ab77da1898f37009fb27
Rohan <rohansinghbsr001@gmail.com>
2026-09-16 21:38:29 +0530

    Time: 3 ms (86.03%), Space: 43.5 MB (25.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,69 @@
 class Solution {
     public boolean isValid(String s) {
 
-        if(s.length()%2!=0){
+        //both are completly same but uncommented code is more clear
+        // if(s.length()%2!=0){
+        //     return false;
+        // }
+        // Stack<Character> st=new Stack<>();
+
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+        //         st.push(s.charAt(i));
+        //     }
+
+        //     else  if(st.size()==0){
+        //         return false;
+        //     }
+
+        //     else if((st.peek()=='(' && s.charAt(i)==')')
+        //         || (st.peek()=='[' && s.charAt(i)==']')
+        //         || (st.peek()=='{' && s.charAt(i)=='}')){
+
+        //             st.pop();
+        //     }
+
+        //     else{
+        //         return false;
+        //     }
+        // }
+        // return st.size()==0;
+
+
+        int n=s.length();
+        if(n%2!=0){
             return false;
         }
-        Stack<Character> st=new Stack<>();
-
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
-                st.push(s.charAt(i));
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                stack.push(ch);
             }
-
-            else  if(st.size()==0){
-                return false;
-            }
-
-            else if((st.peek()=='(' && s.charAt(i)==')')
-                || (st.peek()=='[' && s.charAt(i)==']')
-                || (st.peek()=='{' && s.charAt(i)=='}')){
-
-                    st.pop();
-            }
-
             else{
-                return false;
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char top=stack.peek();
+                if(ch==')' && top!='('){
+                    return false;
+                }
+
+                else if(ch==']' && top!='['){
+                    return false;
+                }
+
+                else if(ch=='}' && top!='{'){
+                    return false;
+                }
+                else{
+                    stack.pop();
+                }
             }
-
-
         }
-        return st.size()==0;
+
+        return stack.isEmpty();
         
     }
 }
