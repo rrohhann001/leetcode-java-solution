1b6b6e58a60446f6f8d3a18a73289310120dafef
Rohan <rohansinghbsr001@gmail.com>
2026-09-20 11:27:43 +0530

    Time: 31 ms (83.07%), Space: 105.2 MB (5.02%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,99 +1,104 @@
-// class MinStack {
-//     List<Integer> stack;
-//     int top;
-
-//     List<Integer> minStack;
-//     int minTop;
-
-//     public MinStack() {
-//         stack=new ArrayList<>();
-//         top=-1;
-
-//         minStack=new ArrayList<>();
-//         minTop=-1;
-//     }
-    
-//     public void push(int value) {
-//         top++;
-//         stack.add(top,value);
-
-//         if(minTop<0 || value<=minStack.get(minTop)){
-//             minTop++;
-//             minStack.add(minTop,value);
-//         }
-        
-//     }
-    
-//     public void pop() {
-//         if(minTop<0 || top<0){
-//             return;
-//         }
-//         // else if(stack.get(top)==(minStack.get(minTop))) { ye Integer object ke case mai ye value copaire nahi karta hun ka reference check karta hai
-//         else if(stack.get(top).equals(minStack.get(minTop)))  {
-//             minTop--;
-//         }
-//         top--;
-        
-//     }
-    
-//     public int top() {
-//         if(top>=0){
-//             return stack.get(top);
-//         }
-//         return Integer.MIN_VALUE;
-        
-//     }
-    
-//     public int getMin() {
-//        if(minTop<0){
-//             return Integer.MAX_VALUE;
-//        }
-//        return minStack.get(minTop);
-//     }
-
+//this beats 83.07% and runtime 31ms in this approch I use ArrayList
 class MinStack {
-    Stack<Integer> stack;
+    List<Integer> stack;
+    int top;
 
-    Stack<Integer> minStack;
+    List<Integer> minStack;
+    int minTop;
 
     public MinStack() {
-        stack=new Stack<>();
+        stack=new ArrayList<>();
+        top=-1;
 
-        minStack=new Stack<>();
+        minStack=new ArrayList<>();
+        minTop=-1;
     }
     
     public void push(int value) {
-        stack.push(value);
+        top++;
+        stack.add(top,value);
 
-        if(minStack.isEmpty() || value<=minStack.peek()){
-            minStack.push(value);
+        if(minTop<0 || value<=minStack.get(minTop)){
+            minTop++;
+            minStack.add(minTop,value);
         }
         
     }
     
     public void pop() {
-       if(!minStack.isEmpty() && minStack.peek().equals(stack.peek())){
-           minStack.pop();
-       }
-       stack.pop();
+        if(minTop<0 || top<0){
+            return;
+        }
+        // else if(stack.get(top)==(minStack.get(minTop))) { ye Integer object ke case mai ye value copaire nahi karta hun ka reference check karta hai
+        else if(stack.get(top).equals(minStack.get(minTop)))  {
+            minTop--;
+        }
+        top--;
         
     }
     
     public int top() {
-        if(stack.isEmpty()){
-            return Integer.MIN_VALUE;
+        if(top>=0){
+            return stack.get(top);
         }
-        return stack.peek();
+        return Integer.MIN_VALUE;
         
     }
     
     public int getMin() {
-       if(minStack.isEmpty()){
+       if(minTop<0){
             return Integer.MAX_VALUE;
        }
-       return minStack.peek();
+       return minStack.get(minTop);
     }
 }
+
+
+//this is my second approch in which i use stack and this approch beats 7.63% and runtime 39ms
+
+// class MinStack {
+//     Stack<Integer> stack;
+
+//     Stack<Integer> minStack;
+
+//     public MinStack() {
+//         stack=new Stack<>();
+
+//         minStack=new Stack<>();
+//     }
+    
+//     public void push(int value) {
+//         stack.push(value);
+
+//         if(minStack.isEmpty() || value<=minStack.peek()){
+//             minStack.push(value);
+//         }
+        
+//     }
+    
+//     public void pop() {
+//        if(!minStack.isEmpty() && minStack.peek().equals(stack.peek())){
+//            minStack.pop();
+//        }
+//        stack.pop();
+        
+//     }
+    
+//     public int top() {
+//         if(stack.isEmpty()){
+//             return Integer.MIN_VALUE;
+//         }
+//         return stack.peek();
+        
+//     }
+    
+//     public int getMin() {
+//        if(minStack.isEmpty()){
+//             return Integer.MAX_VALUE;
+//        }
+//        return minStack.peek();
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
