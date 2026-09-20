7d07f2c42c0bd7e944c15044ed72371bb6df9182
Rohan <rohansinghbsr001@gmail.com>
2026-09-20 11:24:41 +0530

    Time: 39 ms (7.63%), Space: 100.1 MB (43.94%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,54 +1,97 @@
-class MinStack {
-    List<Integer> stack;
-    int top;
+// class MinStack {
+//     List<Integer> stack;
+//     int top;
 
-    List<Integer> minStack;
-    int minTop;
+//     List<Integer> minStack;
+//     int minTop;
+
+//     public MinStack() {
+//         stack=new ArrayList<>();
+//         top=-1;
+
+//         minStack=new ArrayList<>();
+//         minTop=-1;
+//     }
+    
+//     public void push(int value) {
+//         top++;
+//         stack.add(top,value);
+
+//         if(minTop<0 || value<=minStack.get(minTop)){
+//             minTop++;
+//             minStack.add(minTop,value);
+//         }
+        
+//     }
+    
+//     public void pop() {
+//         if(minTop<0 || top<0){
+//             return;
+//         }
+//         // else if(stack.get(top)==(minStack.get(minTop))) { ye Integer object ke case mai ye value copaire nahi karta hun ka reference check karta hai
+//         else if(stack.get(top).equals(minStack.get(minTop)))  {
+//             minTop--;
+//         }
+//         top--;
+        
+//     }
+    
+//     public int top() {
+//         if(top>=0){
+//             return stack.get(top);
+//         }
+//         return Integer.MIN_VALUE;
+        
+//     }
+    
+//     public int getMin() {
+//        if(minTop<0){
+//             return Integer.MAX_VALUE;
+//        }
+//        return minStack.get(minTop);
+//     }
+
+class MinStack {
+    Stack<Integer> stack;
+
+    Stack<Integer> minStack;
 
     public MinStack() {
-        stack=new ArrayList<>();
-        top=-1;
+        stack=new Stack<>();
 
-        minStack=new ArrayList<>();
-        minTop=-1;
+        minStack=new Stack<>();
     }
     
     public void push(int value) {
-        top++;
-        stack.add(top,value);
+        stack.push(value);
 
-        if(minTop<0 || value<=minStack.get(minTop)){
-            minTop++;
-            minStack.add(minTop,value);
+        if(minStack.isEmpty() || value<=minStack.peek()){
+            minStack.push(value);
         }
         
     }
     
     public void pop() {
-        if(minTop<0 || top<0){
-            return;
-        }
-        // else if(stack.get(top)==(minStack.get(minTop))) { ye Integer object ke case mai ye value copaire nahi karta hun ka reference check karta hai
-        else if(stack.get(top).equals(minStack.get(minTop)))  {
-            minTop--;
-        }
-        top--;
+       if(!minStack.isEmpty() && minStack.peek().equals(stack.peek())){
+           minStack.pop();
+       }
+       stack.pop();
         
     }
     
     public int top() {
-        if(top>=0){
-            return stack.get(top);
+        if(stack.isEmpty()){
+            return Integer.MIN_VALUE;
         }
-        return Integer.MIN_VALUE;
+        return stack.peek();
         
     }
     
     public int getMin() {
-       if(minTop<0){
+       if(minStack.isEmpty()){
             return Integer.MAX_VALUE;
        }
-       return minStack.get(minTop);
+       return minStack.peek();
     }
 }
 
