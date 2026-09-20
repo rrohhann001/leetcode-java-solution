8959806b9adbadabef11105d5a9d26028e31137d
Rohan <rohansinghbsr001@gmail.com>
2026-09-20 11:13:29 +0530

    Time: 31 ms (83.07%), Space: 105.3 MB (5.02%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,62 @@
+class MinStack {
+    List<Integer> stack;
+    int top;
+
+    List<Integer> minStack;
+    int minTop;
+
+    public MinStack() {
+        stack=new ArrayList<>();
+        top=-1;
+
+        minStack=new ArrayList<>();
+        minTop=-1;
+    }
+    
+    public void push(int value) {
+        top++;
+        stack.add(top,value);
+
+        if(minTop<0 || value<=minStack.get(minTop)){
+            minTop++;
+            minStack.add(minTop,value);
+        }
+        
+    }
+    
+    public void pop() {
+        if(minTop<0 || top<0){
+            return;
+        }
+        // else if(stack.get(top)==(minStack.get(minTop))) { ye Integer object ke case mai ye value copaire nahi karta hun ka reference check karta hai
+        else if(stack.get(top).equals(minStack.get(minTop)))  {
+            minTop--;
+        }
+        top--;
+        
+    }
+    
+    public int top() {
+        if(top>=0){
+            return stack.get(top);
+        }
+        return Integer.MIN_VALUE;
+        
+    }
+    
+    public int getMin() {
+       if(minTop<0){
+            return Integer.MAX_VALUE;
+       }
+       return minStack.get(minTop);
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
