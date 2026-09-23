117ed7ebe22dfe63789c30b7e1a40ed0e8f2cb6d
Rohan <rohansinghbsr001@gmail.com>
2026-09-23 23:39:14 +0530

    Time: 5 ms (98.65%), Space: 130.4 MB (76.07%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -10,30 +10,61 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        Stack<ListNode> stack=new Stack<>();
-        //insert all node into the stack
-        ListNode temp=head;
-        while(temp!=null){
-            stack.push(temp);
-            temp=temp.next;
+
+        //this approch beats 44.85% and runtime 41ms. In this I use Stack
+
+        // Stack<ListNode> stack=new Stack<>();
+        // //insert all node into the stack
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     stack.push(temp);
+        //     temp=temp.next;
+        // }
+
+        // //create a list
+        // ListNode newHead=stack.pop();
+        // int max=newHead.val;
+        // while(!stack.isEmpty()){
+        //     ListNode node=stack.pop();
+        //     if(node.val<max){
+        //         continue;
+        //     }
+        //     else{
+        //         //insert at head
+        //         node.next=newHead;
+        //         newHead=node;
+        //         max=node.val;
+        //     }
+        // }
+
+        // return newHead;
+
+
+        //without stack
+        //reverse the linked list
+        ListNode current=head;
+        ListNode pre=null;
+        while(current!=null){
+            ListNode nextNode=current.next;
+            current.next=pre;
+            pre=current;
+            current=nextNode;
         }
 
         //create a list
-        ListNode newHead=stack.pop();
-        int max=newHead.val;
-        while(!stack.isEmpty()){
-            ListNode node=stack.pop();
-            if(node.val<max){
-                continue;
-            }
-            else{
-                //insert at head
+        ListNode newHead=new ListNode(pre.val);
+        current=pre.next;
+        while(current!=null){
+            if(current.val>=newHead.val){
+                ListNode node=new ListNode(current.val);
                 node.next=newHead;
                 newHead=node;
-                max=node.val;
             }
+            current=current.next;
         }
 
         return newHead;
+
+
     }
 }
